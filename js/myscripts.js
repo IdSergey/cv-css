b4284cebfb37912ad41f95fcc194d446564e5c50
IdSergey <saydimir@gmail.com>
2021-12-15 20:21:39 +0200

    minor change

---
+++ js/myscripts.js
@@ -0,0 +1,13 @@
+function toggle(name) {
+  let w = document.getElementById(name);
+    if (w.style.display == 'none') {
+      w.style.display='block';
+    }
+    else { 
+      w.style.display='none';
+      // w.addClass("opacity");
+      //='none';
+
+      
+  }
+}
