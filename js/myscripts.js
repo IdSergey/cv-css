ccaa95412c81f036bb7ecadf6cdbe1d0f288fda0
IdSergey <saydimir@gmail.com>
2021-12-16 16:38:56 +0200

    css media

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,13 +1,10 @@
 function toggle(name) {
   let w = document.getElementById(name);
-    if (w.style.display == 'none') {
-      w.style.display='block';
-    }
-    else { 
-      w.style.display='none';
-      // w.addClass("opacity");
-      //='none';
-
-      
+  if (w.style.display == "none") {
+    w.style.display = "block";
+  } else {
+    w.style.display = "none";
+    // w.addClass("opacity");
+    //='none';
   }
 }
