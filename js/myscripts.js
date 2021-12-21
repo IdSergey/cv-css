939bf0ee511396e1993afe6f92d09b0c832c1f72
IdSergey <saydimir@gmail.com>
2021-12-21 23:37:32 +0200

    send.php

--- js/myscripts.js
+++ js/myscripts.js
@@ -8,3 +8,39 @@ function toggle(name) {
     //='none';
   }
 }
+
+//  Обрботка форсы
+
+$('.myform').submit(function (e) {
+  e = preventDefault();
+  let th = $(this);
+  let mess = $('.myform');
+
+  $.ajax( {
+
+    url: "send.php",
+    type: "post",
+    data: th.serialize(),
+    success: function (data) {
+      if( data == 1) {
+        mess.html("Email введен не верно");
+        return false;
+
+      }
+      mess.html("Спасибо. Ваше сообщение отправлено.");
+      
+    },error: function () {
+      mess.html("Ошибка отправки.");
+      
+    }
+
+  }
+
+  )
+
+
+}
+
+function () {
+    
+  })
